47478b6a30836a60f0da6e50f265353c1c1cbb18
sarthak-goel <sarthakgoeliitkgp@gmail.com>
2024-08-18 12:55:05 +0530

    data added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.mlopsP import logger
+from mlopsP.pipeline.data_ingestion_1 import DataIngestionTrainPipeline
 
-logger.info("custom loggging")
+STAGE_NAME="Data Ingestion"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
